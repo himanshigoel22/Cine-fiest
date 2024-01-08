0bbedee1fc9f479714ac16e930f7d4323260989d
ParasGoel1234 <paras.goel358@gmail.com>
2024-01-08 22:47:28 +0530

    first commit

--- src/pages/404/notFound.jsx
+++ src/pages/404/notFound.jsx
@@ -3,7 +3,12 @@ import "./style.scss";
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className="NotFound">
+    <ContentWrapper>
+        <span className="bigText">404</span>
+        <span className="smallText">Page not found!</span>
+    </ContentWrapper>
+</div>
   )
 };
 
